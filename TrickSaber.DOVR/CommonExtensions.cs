a9d3e2c519aad100fbf2ebcb7885ed8ec32d20c0
Toni Macaroni <tonimacaroniy@gmail.com>
2020-04-20 15:28:22 +0200

    renamed project to TrickSaber.DOVR

---
+++ TrickSaber.DOVR/CommonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using TrickSaber.DOVR;
+
+namespace TrickSaber
+{
+    public static class CommonExtensions
+    {
+        public static void Log(this object message)
+        {
+            Plugin.Log.Debug(message.ToString());
+        }
+
+        public static string GetVersionString(this SemVer.Version version)
+        {
+            return version.Major + "." + version.Minor + "." + version.Patch;
+        }
+    }
+}
