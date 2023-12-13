b5119ef574a484b19aa3b192117c8ba02400948f
Arimodu <44728973+Arimodu@users.noreply.github.com>
2023-12-13 16:50:06 +0100

    Fix my stupidity

--- TrickSaber/Plugin.cs
+++ TrickSaber/Plugin.cs
@@ -12,8 +12,6 @@ namespace TrickSaber
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
-        [Inject] internal static CoroutineStarter SharedCoroutineStarter;
-
         [Init]
         public Plugin(IPALogger logger, Config conf, Zenjector zenjector)
         {
@@ -21,7 +19,6 @@ namespace TrickSaber
 
             zenjector.UseLogger(logger);
             zenjector.UseHttpService();
-            zenjector.Install(Location.App, Container => Container.BindInstance(this).AsSingle());
             zenjector.Install<AppInstaller>(Location.App, pluginConfig);
             zenjector.Install<MenuInstaller>(Location.Menu);
             zenjector.Install<GameInstaller>(Location.StandardPlayer);
